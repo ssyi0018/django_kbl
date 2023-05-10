a173b56f498aa5a87f3f5c03b7d488deafe146a3
ssyi <tidussy@qq.com>
2023-05-10 20:27:50 +0800

    ssyi

--- django_kbl/urls.py
+++ django_kbl/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('index/', views.index),
     path('user/list/', views.user_list),
     path('user/add/', views.user_add),
+    path('tpl', views.tpl),
 ]
--- scApp/views.py
+++ scApp/views.py
@@ -12,3 +12,17 @@ def user_list(request):
 
 def user_add(request):
     return render(request, 'user_add.html')
+
+
+def tpl(request):
+    name = 'ssyi'
+    roles = ['管理员', 'CEO', '保安']
+    user_info = {'name': 'ssyi', 'salary': 100000, 'role': 'CTO'}
+
+    data_list = [
+        {'name': 'ssyi01', 'salary': 200000, 'role': 'COO'},
+        {'name': 'ssyi02', 'salary': 400000, 'role': 'CAO'},
+        {'name': 'ssyi03', 'salary': 500000, 'role': 'CWO'},
+    ]
+
+    return render(request, 'tpl.html', {'n1': name, 'n2': roles, 'n3': user_info, 'n4': data_list})
