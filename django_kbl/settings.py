ac953ce1fa24179aa427c764ce72783e9eb3b193
ssyi <tidussy@qq.com>
2023-05-11 17:21:39 +0800

    0511

--- django_kbl/settings.py
+++ django_kbl/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'django_kbl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kblsc',
+        'USER': 'root',
+        'PASSWORD': '000018',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
