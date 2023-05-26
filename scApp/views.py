369f7ecf447378bda634a18aeab4aa285a59cf3c
ssyi <tidussy@qq.com>
2023-05-26 17:14:44 +0800

    0526

--- scApp/views.py
+++ scApp/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, HttpResponse, redirect
-import requests
+import requests,copy
 from scApp import models
 from pptx import Presentation
 from django import forms
 from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
 from django.utils.safestring import mark_safe
+from scApp.utils.pagination import Pagination
 
 
 # Create your views here.
@@ -217,85 +218,94 @@ def user_list(request):
     if search_data:
         data_dict['name__contains'] = search_data
 
-    # 分页
-    try:
-        page = int(request.GET.get('page', 1))  # 分页
-    except  ValueError:
-        page = 1
-    page_size = 10  # 每页显示数据
-    start = (page - 1) * page_size
-    end = page * page_size
+    # 搜索条件
+    queryset = models.UserInfo.objects.filter(**data_dict).order_by('id')
+
+    # 调用分页封装函数,实例化对象
+    page_object = Pagination(request, queryset,page_size=9)
+
+    # 定义一个字典
+    context = {'queryset01': page_object.page_queryset, 'search_data': search_data,
+               'page_string': page_object.html()}
+    # 当前页
+    # try:
+    #     page = int(request.GET.get('page', 1))  # 分页
+    # except  ValueError:
+    #     page = 1
+    # page_size = 10  # 每页显示数据
+    # start = (page - 1) * page_size
+    # end = page * page_size
 
     # 数据总条数
-    total_num = models.UserInfo.objects.filter(**data_dict).order_by('id').count()
+    # total_num = models.UserInfo.objects.filter(**data_dict).order_by('id').count()
+    #
+    # # 计算出总页码 divmod函数计算商和余数
+    # total_page, div = divmod(total_num, page_object.page_size)
+    # if div:
+    #     total_page += 1
 
-    # 计算出总页码 divmod函数计算商和余数
-    total_page, div = divmod(total_num, page_size)
-    if div:
-        total_page += 1
-
-    # 根据当前页计算出前后5页
-    plus = 5
-    if total_page <= 2 * plus + 1:
-        # 当数据库数据少的时候，没有达到11页
-        start_page = 1
-        end_page = total_page
-    else:
-        # 数据库数据比较多，判断当前页,<5
-        if page <= plus:
-            start_page = 1
-            end_page = 2 * plus
-        else:
-            # 当前页大于5
-            if (page + plus) > total_page:
-                start_page = total_page - 2 * plus
-                end_page = total_page
-            else:
-                start_page = page - plus
-                end_page = page + plus
-
-    # 页码
-    page_list = []
-    page_list.append('<li><a href="?page={}">首页</a></li>'.format(1))
-    # 上一页
-    if page > 1:
-        prev = '<li><a href="?page={}">上一页</a></li>'.format(page - 1)
-    else:
-        prev = '<li><a href="?page={}">上一页</a></li>'.format(1)
-    page_list.append(prev)
-
-    # 分页页面
-    for i in range(start_page, end_page + 1):
-        if i == page:
-            ele = '<li class="active"><a href="?page={}">{}</a></li>'.format(i, i)
-        else:
-            ele = '<li><a href="?page={}">{}</a></li>'.format(i, i)
-        page_list.append(ele)
-
-    # 下一页
-    if page < total_page:
-        prev = '<li><a href="?page={}">下一页</a></li>'.format(page + 1)
-    else:
-        prev = '<li><a href="?page={}">下一页</a></li>'.format(total_page)
-    page_list.append(prev)
-
-    page_list.append('<li><a href="?page={}">尾页</a></li>'.format(total_page))
-
-    search_tz = '''
-                 <li>
-                    <form style="float: left;margin-left: -1px" method="get">
-                        <input name="page"
-                               style="position: relative;float: left;display: inline-block;width: 80px;border-radius: 0;"
-                               type="text" class="form-control" placeholder="页码">
-                        <button style="border-radius: 0" class="btn btn-default" type="submit">跳转</button>
-                        </span>
-                    </form>
-                </li>
-    '''
-    page_list.append(search_tz)
-
-    # 导入mark_safe，把数据包裹成安全的传递给html
-    page_string = mark_safe(''.join(page_list))
+    # # 根据当前页计算出前后5页
+    # plus = 5
+    # if total_page <= 2 * plus + 1:
+    #     # 当数据库数据少的时候，没有达到11页
+    #     start_page = 1
+    #     end_page = total_page
+    # else:
+    #     # 数据库数据比较多，判断当前页,<5
+    #     if page <= plus:
+    #         start_page = 1
+    #         end_page = 2 * plus
+    #     else:
+    #         # 当前页大于5
+    #         if (page + plus) > total_page:
+    #             start_page = total_page - 2 * plus
+    #             end_page = total_page
+    #         else:
+    #             start_page = page - plus
+    #             end_page = page + plus
+    #
+    # # 页码
+    # page_list = []
+    # page_list.append('<li><a href="?page={}">首页</a></li>'.format(1))
+    # # 上一页
+    # if page > 1:
+    #     prev = '<li><a href="?page={}">上一页</a></li>'.format(page - 1)
+    # else:
+    #     prev = '<li><a href="?page={}">上一页</a></li>'.format(1)
+    # page_list.append(prev)
+    #
+    # # 分页页面
+    # for i in range(start_page, end_page + 1):
+    #     if i == page:
+    #         ele = '<li class="active"><a href="?page={}">{}</a></li>'.format(i, i)
+    #     else:
+    #         ele = '<li><a href="?page={}">{}</a></li>'.format(i, i)
+    #     page_list.append(ele)
+    #
+    # # 下一页
+    # if page < total_page:
+    #     prev = '<li><a href="?page={}">下一页</a></li>'.format(page + 1)
+    # else:
+    #     prev = '<li><a href="?page={}">下一页</a></li>'.format(total_page)
+    # page_list.append(prev)
+    #
+    # page_list.append('<li><a href="?page={}">尾页</a></li>'.format(total_page))
+    #
+    # search_tz = '''
+    #              <li>
+    #                 <form style="float: left;margin-left: -1px" method="get">
+    #                     <input name="page"
+    #                            style="position: relative;float: left;display: inline-block;width: 80px;border-radius: 0;"
+    #                            type="text" class="form-control" placeholder="页码">
+    #                     <button style="border-radius: 0" class="btn btn-default" type="submit">跳转</button>
+    #                     </span>
+    #                 </form>
+    #             </li>
+    # '''
+    # page_list.append(search_tz)
+    #
+    # # 导入mark_safe，把数据包裹成安全的传递给html
+    # page_string = mark_safe(''.join(page_list))
 
     # queryset = models.UserInfo.objects.filter(name__contains=search_data)
     # print(queryset)
@@ -306,10 +316,9 @@ def user_list(request):
     #     print(obj.create_time.strftime('%Y-%m-%d'), obj.get_gender_display(),
     #           obj.depart.title
     #           )
-    queryset = models.UserInfo.objects.filter(**data_dict).order_by('id')[start:end]
+    # queryset = models.UserInfo.objects.filter(**data_dict).order_by('id')[page_object.start:page_object.end]
 
-    return render(request, 'user_list.html', {'queryset01': queryset, 'search_data': search_data,
-                                              'page_string': page_string})
+    return render(request, 'user_list.html', context)
 
 
 def user_add(request):
