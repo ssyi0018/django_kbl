56d58303d9af94d2c381f3dd3f39a23a237d7c63
ssyi <tidussy@qq.com>
2023-05-30 09:05:06 +0800

    0529

--- scApp/views/account.py
+++ scApp/views/account.py
@@ -15,6 +15,10 @@ class LoginForm(BootStrapForm):
     password = forms.CharField(label='密码',
                                widget=forms.PasswordInput(render_value=True),
                                required=True, )
+    # 验证码字段自定义
+    code = forms.CharField(label='验证码',
+                           widget=forms.TextInput,
+                           required=True, )
 
     def clean_password(self):
         pwd = self.cleaned_data.get('password')
@@ -28,6 +32,12 @@ def login(request):
     else:
         form = LoginForm(data=request.POST)
         if form.is_valid():
+            # 验证码校验
+            user_input_code = form.cleaned_data.pop('code')
+            image_code = request.session.get('image_code', '')
+            if image_code.upper() != user_input_code.upper():
+                form.add_error('code', '验证码输入错误')
+                return render(request, 'login.html', {'form': form})
             # print(form.cleaned_data)
             # 钩子方法里验证成功，获取用户名和密码
             # 直接在models里写
@@ -41,6 +51,8 @@ def login(request):
                 return render(request, 'login.html', {'form': form})
             # 登录成功，cookie和session处理
             request.session['info'] = {'id': admin_object.id, 'name': admin_object.username}
+            # 1天免登陆，session保存1天
+            request.session.set_expiry(60 * 60 * 24)
             return redirect('/admin/list')
         return render(request, 'login.html', {'form': form})
 
@@ -48,6 +60,11 @@ def login(request):
 def image_code(request):
     # 调用图片验证码
     img, code_string = check_code()
+
+    # 写入到session，用于后面获取
+    request.session['image_code'] = code_string
+    # 设置session超时60秒
+    request.session.set_expiry(60)
     # 创建一个内存文件
     stream = BytesIO()
     # 图片写入内存中
